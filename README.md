572f35479c40b8acfbe832221a47dc6eaa4db469
Tenzin Crouch <tenzincrouch@gmail.com>
2022-07-23 16:02:29 -0400

    added initial commit of code from Newwie website

---
+++ README.md
@@ -0,0 +1,31 @@
+<img 
+  align="right"
+  width="1600px"
+  alt="Newwwie Logo"
+  src="newwwie-logo.svg?sanitize=true" 
+/>
+
+# newwwie.com
+
+The source for [newwwie.com](https://newwwie.com). The project is really barebones to allow for a new member to not be lost in tooling and encourage contributions. 
+
+It takes a community to build the community.
+
+## Commands
+
+### Get the project going
+`npm install`
+
+### Run hot reloading development environment
+`npm run dev`
+
+### Build for production
+`npm run build`
+
+## Event feed
+
+Pulls from the Meetup API based on a list of approved Meetup Group IDs. 
+
+### Add a meetup group to the event feed
+
+[Create an issue](https://github.com/newwwie/newwwie.com/issues/new?title=Request%20for%20new%20Meetup%20to%20be%20added%20to%20event%20stream&body=**Meetup%20Group%20URL:**%20**Meetup%20relevance:**) and we'll update accordingly.
---
+++ src/js/events/events-data.js
@@ -0,0 +1,129 @@
+// Auto Generated on 2022-07-23T13:33:09.407Z
+ module.exports = [
+  {
+    "event": {
+      "title": "July Meeting: Cole Cornford on JavaScript Security",
+      "description": "This month, we are most fortunate to have **Cole Cornford** speaking to us about JavaScript security.\n\n**Security concerns for the Web's favourite language!**\n\nJavaScript is now the most popular client and server-side language in use in 2022. It's a big target for adversaries, and there are a tremendous amount of ways that they work to get code execution happening in your app.\n\nJoin Cole to learn about the techniques adversaries use to gain script injection, what you can do about those, and what you can do to build safer web applications.\n\nCole Cornford is the Director of Galah Cyber, an Australian Cybersecurity consultancy with a focus on Application, Cloud, and Product Security. Cole has worked in the security space for 8 years across a variety of industries and cybersecurity fields. Cole helps organise a few other Newcastle communities, as well as being a regular presenter at AISA, BSides, and OWASP Conferences.\n\nDate: Wednesday 27th of July\nLocation: Mudbath\nUnit 8 (Upstairs) 710 Hunter Street. Entry via the left side of the building.\nLivestream: https://livestream.mudbath.com.au/\n\nOff-street parking is available directly behind the offices, entry is via the ibis hotel driveway.\n\nThanks to the Newy Tech People for sponsoring pizza.\n---\n\nDon't be insecure, come along!",
+      "dateTime": "2022-07-27T18:00+10:00",
+      "eventUrl": "https://www.meetup.com/newcastlejs-javascript-meetup/events/287320127",
+      "going": 10,
+      "maxTickets": 50,
+      "duration": "PT3H",
+      "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/473023676/676x380.webp",
+      "venue": {
+        "name": "710 Hunter St",
+        "lat": -32.926094,
+        "lng": 151.76259,
+        "address": "710 Hunter St",
+        "city": "Newcastle West"
+      }
+    },
+    "group": {
+      "name": "NewcastleJS - JavaScript Meetup",
+      "urlname": "newcastlejs-javascript-meetup",
+      "groupPhoto": {
+        "id": "473023676",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      },
+      "logo": {
+        "id": "473023676",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      }
+    }
+  },
+  {
+    "event": {
+      "title": "NCSG, July 2022 | Andrew Stace",
+      "description": "Hello friends! Welcome back to another instalment of NCSG.\n\nThis month, we're lucky to have Andrew Stace, the fearless leader and founder of our lovely group. He currently works as a cybersecurity professional for an ASX-listed company, and the grand finale of his presentation will be him 100%ing a variety of songs on Beat Saber.\n\nSynopsis of Andrew's talk:\n\nCybersecurity scams are increasing in frequency & impact. Meaning that more people than ever before are coming to be impacted by some form of cyber crime.\n\nBut often our attention is focused on the impact being described in financial or technical framings. Instead, in this talk, Andrew will explore some of the emotional impact of cybercrime &/or the emotional context(s) in which it can prosper.\n\n**Event details:**\n\nDate: July 28th, 2022\nTime: 18:00-20:00ish (or until we go to the pub)\nVenue: 16 Honeysuckle Drive, Newcastle (The University of Newcastle 'Q Building') - second floor 'seminar space'\nFood: Provided, please inform us of dietary requirements when RSVPing\nDrink: Non-alcoholic provided, come and grab a few with us at the pub after if you'd like!\n\n**The rest:**\n\nIf you’re experiencing any symptoms representative of COVID-19, please don’t attend. We’ll miss you, but the health/safety of attendees is paramount.\n\nNCSG is a community group determined to improve and uplift the Newcastle cybersecurity community. We meet monthly (on the last Thursday of each month) and have a bunch of interesting speakers, from local legends to heavy hitters in the enterprise world.\n\nWant to learn more? Be sure to follow us on LinkedIn: linkedin.com/company/newcastlecybersecuritygroup/ or visit newwwie.com, join the community and come and chat in the #security channel.",
+      "dateTime": "2022-07-28T18:00+10:00",
+      "eventUrl": "https://www.meetup.com/newcastle-cyber-security-group/events/286991107",
+      "going": 22,
+      "maxTickets": 50,
+      "duration": "PT2H",
+      "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/505335833/676x380.webp",
+      "venue": {
+        "name": "Q Building, The University of Newcastle",
+        "lat": -32.926277,
+        "lng": 151.7683,
+        "address": "16B Honeysuckle Dr",
+        "city": "Newcastle"
+      }
+    },
+    "group": {
+      "name": "Newcastle Cyber Security Group",
+      "urlname": "Newcastle-Cyber-Security-Group",
+      "groupPhoto": {
+        "id": "483205216",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      },
+      "logo": {
+        "id": "483205216",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      }
+    }
+  },
+  {
+    "event": {
+      "title": "August: Speakers Wanted",
+      "description": "Details to come",
+      "dateTime": "2022-08-03T18:00+10:00",
+      "eventUrl": "https://www.meetup.com/newcastle-coders-group/events/286934350",
+      "going": 2,
+      "maxTickets": 0,
+      "duration": "PT2H",
+      "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/505406540/676x380.webp",
+      "venue": null
+    },
+    "group": {
+      "name": "Newcastle Coders Group",
+      "urlname": "Newcastle-Coders-Group",
+      "groupPhoto": {
+        "id": "505406540",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      },
+      "logo": {
+        "id": "505406540",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      }
+    }
+  },
+  {
+    "event": {
+      "title": "data.AUG <- Stats, ML and ops: presented by Team HH",
+      "description": "This month, there's something for everyone with our Trio of Analytics Dips 😋, courtesy of Team Honeysuckle Health:\n\n* **Stats**: Imagine you organised a great big initiative, and it just concluded yesterday. That initiative could be your marketing campaign, staff training course, health intervention, etc.\nYou feel like things went well, but you wish you'd thought about *proving* the success of your initiative with *hard numbers*. But it's all too late, now that the intervention is complete... right?\nFear not! We'll talk about observational studies and look at how they can quantify the success of your intervention, even *after the fact*. ⚖\n* **Machine Learning**: Sometimes you need to compare things that you can't quite measure directly 📏.\nTake, for instance, the cost of the *same* surgery. Though the same procedure was delivered, the price will still depend on the patient's age, where they live, their medical history and risks. How can we tell if a surgery was overpriced... when no two are different? Answer in this talk: it involves ML 🧠\n* **Ops**: Making all of this come together, at scale, for real-time insights.\n\nVENUE:\nThe university's brand new Q building in Honeysuckle!\n\nhttps://goo.gl/maps/35LwcLhKV1nvoFAh7\n\nIMPORTANT this is NOT the NewSpace building where we met last year.\n\nWe will meet in the Level 2 Seminar Space. You'll need to enter through the main doors on Worth Place and will be directed to the elevators to join the meetup.\n\nPARKING: The closest parking is Wright Lane Carpark.\nhttps://goo.gl/maps/rj6Yg8EkpjYzUmbi7\n\nAfter 5pm it is free and is a 400m - 5 minute walk.\n\n##-------------------------------------------------------------------##\n\nTIME: 5.30pm - 7.30pm\n\nFOOD: 🍕 + 🥤\n\n* Wood fired pizza, sponsored by Newy Tech People\n* Please advise dietary requirements in RSVP\n* Soft drinks\n\nSCHEDULE:\n\n* 5:30pm: Networking + Food + Drinks\n(Don't stress if you are held up in traffic)\n* 6:15pm: Presentation\n* 7:15pm: Packing up + Bonus questions\n* 7:30pm: Close",
+      "dateTime": "2022-08-18T17:30+10:00",
+      "eventUrl": "https://www.meetup.com/hunter-data-analytics/events/283702285",
+      "going": 6,
+      "maxTickets": 40,
+      "duration": "PT2H",
+      "imageUrl": "https://secure-content.meetupstatic.com/images/classic-events/501789003/676x380.webp",
+      "venue": {
+        "name": "Q Building, The University of Newcastle",
+        "lat": -32.926277,
+        "lng": 151.7683,
+        "address": "16B Honeysuckle Dr",
+        "city": "Newcastle"
+      }
+    },
+    "group": {
+      "name": "Hunter Data Analytics",
+      "urlname": "hunter-data-analytics",
+      "groupPhoto": {
+        "id": "483576656",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      },
+      "logo": {
+        "id": "483576656",
+        "baseUrl": "https://secure-content.meetupstatic.com/images/classic-events/",
+        "preview": null
+      }
+    }
+  }
+]
